9808ed456c249431b799c3bd7adbc06899bb3ef5
DABronskikh <dima5202@mail.ru>
2022-10-28 22:18:08 +0500

    Правки после ревью

--- src/main/java/InvoiceCalculator.java
+++ src/main/java/InvoiceCalculator.java
@@ -2,17 +2,24 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InvoiceCalculator {
-    ArrayList<Product> products = new ArrayList<>();
-    int countUsers;
+    protected ArrayList<Product> products = new ArrayList<>();
+    protected int countUsers;
+
+    public void start() {
+        this.requestCountUsers();
+        this.addProducts();
+        this.printProducts();
+        this.printResults();
+    }
 
     public void requestCountUsers() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("На скольких человек необходимо разделить счёт?");
 
-        int count;
+        String input;
         while (true) {
-            count = scanner.nextInt();
-            if (count > 1) {
+            input = scanner.nextLine();
+            if (isInt(input) && Integer.parseInt(input) > 1) {
                 break;
             } else {
                 System.out.println("Кол-во должно быть больше 1");
@@ -20,14 +27,42 @@ public class InvoiceCalculator {
             }
         }
 
-        this.countUsers = count;
+        this.countUsers = Integer.parseInt(input);
     }
 
-    public void addProduct(Product product) {
-        this.products.add(product);
+    public void addProducts() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Добавьте новый товар или введите \"Завершить\"");
+            String productTitle;
+            double productPrice;
+            String input;
+
+            System.out.println("Введите название товара:");
+            input = scanner.nextLine();
+            if (checkEnd(input)) {
+                break;
+            }
+            productTitle = input;
+
+            while (true) {
+                System.out.println("Введите цену товара:");
+                input = scanner.nextLine();
+                if (isDouble(input)) {
+                    productPrice = Double.parseDouble(input);
+                    break;
+                }
+                System.out.println("Некорректное значение");
+            }
+
+            Product product = new Product(productTitle, productPrice);
+            this.products.add(product);
+        }
     }
 
     public void printProducts() {
+        System.out.printf("%n -=-=-=-=-=-=-=- %n");
+
         if (this.products.size() == 0) {
             System.out.println("Список товаров пуст...");
         } else {
@@ -78,6 +113,28 @@ public class InvoiceCalculator {
         double avgSum = this.getAvgSum();
         String getRubAddition = this.getRubAddition(avgSum);
         System.out.printf("Итого: %.2f %s на человека%n", avgSum, getRubAddition);
+    }
+
+    public static boolean isInt(String input) throws NumberFormatException {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isDouble(String input) throws NumberFormatException {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean checkEnd(String input) {
+        return "завершить".equals(input.trim().toLowerCase());
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,6 @@
-import java.util.Scanner;
-
 class Main {
-
-    public static boolean checkEnd(String input) {
-        return "завершить".equals(input.trim().toLowerCase());
-    }
-
     public static void main(String[] args) {
         InvoiceCalculator invoiceCalculator = new InvoiceCalculator();
-        invoiceCalculator.requestCountUsers();
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Добавьте новый товар или введите \"Завершить\"");
-            String productTitle;
-            double productPrice;
-            String input;
-
-            System.out.println("Введите название товара:");
-            input = scanner.nextLine();
-            if (checkEnd(input)) {
-                break;
-            }
-            productTitle = input;
-
-            System.out.println("Введите цену товара:");
-            input = scanner.nextLine();
-            if (checkEnd(input)) {
-                break;
-            }
-            productPrice = Double.parseDouble(input);
-
-            Product product = new Product(productTitle, productPrice);
-            invoiceCalculator.addProduct(product);
-        }
-
-        invoiceCalculator.printProducts();
-        invoiceCalculator.printResults();
+        invoiceCalculator.start();
     }
 }
